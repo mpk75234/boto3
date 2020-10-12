68f4e9a68345c3487aeef54f6903890ce5ad6df2
mpk75234 <mikekennedy75234@gmail.com>
2020-10-12 10:30:37 -0500

    messing around with exceptions

--- IAM/groups/update_group.py
+++ IAM/groups/update_group.py
@@ -8,4 +8,7 @@ generate IAM client
 '''
 iam = boto3.client('iam')
 
-iam.update_group(GroupName='core',NewGroupName='hardcore')
+try:
+    iam.update_group(GroupName='notepad_gurus',NewGroupName='hardcore')
+except:
+    print(f"NOT DERE....{e}")
