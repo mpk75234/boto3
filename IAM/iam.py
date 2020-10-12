ee672679e60ffbdaccedd2217be0c9686731a024
mpk75234 <mikekennedy75234@gmail.com>
2020-10-12 09:40:48 -0500

    OOP IAM user CRUD factory

---
+++ IAM/iam.py
@@ -0,0 +1,25 @@
+'''
+IAM user generator with all bunny-slope CRUD functions
+'''
+import boto3
+class IAM:
+    def __init__(self,name):
+        self.name = name
+        self.iam = boto3.client('iam')
+    def c(self):
+        self.iam.create_user(UserName=self.name)
+    def r(self):
+        reetz = self.iam.list_users()
+        print(reetz)
+    def u(self,nuname):
+        self.iam.update_user(NewUserName=nuname, UserName=self.name)
+        self.r()
+    def d(self):
+        self.iam.delete_user(UserName=self.name)
+        self.r()
+
+
+if __name__ == '__main__':
+    carlos = IAM('carlos')
+    carlos.c()
+    carlos.r()
